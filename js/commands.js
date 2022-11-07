179f9e236e7d468c34eb50fa5cb49301508e963b
Ryan <107238211+Ryanbyte@users.noreply.github.com>
2022-11-07 17:15:33 +0600

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,72 @@
+var twitter = "https://www.twitter.com/Ryanbyte/";
+var password = "youarecrazyman1111";
+var github = "https://github.com/Ryanbyte/";
+var email = 'mailto:abdullahrayyanrahman1@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Rayyan!👋",
+  "I'm a 12 year old programmer and Student",
+  "A person who hates himself the most",
+  "I am not allowed to disclose that information",
+  "... i dont know who that is",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Well, Go ask yourself. Its the biggest mystrey in the universe",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/Ryanbyte' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/Ryanbyte' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Not possible to see here! Goto my Github",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>         Who is Rayyan?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>          You are honestly using this right now',
+  '<span class="command">email</span>          Dont you dare email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  ' .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .-----------------.',
+  '| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |',
+  '| |  _______     | || |      __      | || |  ____  ____  | || |  ____  ____  | || |      __      | || | ____  _____  | |',
+  '| | |_   __ \    | || |     /  \     | || | |_  _||_  _| | || | |_  _||_  _| | || |     /  \     | || ||_   \|_   _| | |',
+  '| |   | |__) |   | || |    / /\ \    | || |   \ \  / /   | || |   \ \  / /   | || |    / /\ \    | || |  |   \ | |   | |',
+  '| |   |  __ /    | || |   / ____ \   | || |    \ \/ /    | || |    \ \/ /    | || |   / ____ \   | || |  | |\ \| |   | |',
+  '| |  _| |  \ \_  | || | _/ /    \ \_ | || |    _|  |_    | || |    _|  |_    | || | _/ /    \ \_ | || | _| |_\   |_  | |',
+  '| | |____| |___| | || ||____|  |____|| || |   |______|   | || |   |______|   | || ||____|  |____|| || ||_____|\____| | |',
+  '| |              | || |              | || |              | || |              | || |              | || |              | |',
+ , 
+ ,
+
+  '<span class="index">TriumvirateStudios. All knights reserved.</span>',
+  '<span class="color2">Welcome to my web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
